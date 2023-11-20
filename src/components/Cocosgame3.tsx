f14c80613b2e32632c2e34c7bfe84a2de76b1c16
nglsena0722c <nglsena0722@com2us.com>
2023-11-20 10:23:09 +0900

    test

--- src/components/Cocosgame3.tsx
+++ src/components/Cocosgame3.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import * as crypto from 'crypto';
 
 export default function Cocosgame2() {
-  const test = async () => {
-    const encoder = new TextEncoder();
-    const data = encoder.encode("hihi");
-    const hash = await window.crypto.subtle.digest("SHA-256", data);
-    console.log('test1')
-    console.log('isit ok?', hash);
-  };
-  test();
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      const test = async () => {
+        const encoder = new TextEncoder();
+        const data = encoder.encode("hihi");
+        const hash = await window.crypto.subtle.digest("SHA-256", data);
+        console.log('test1')
+        console.log('isit ok?', hash);
+      };
+      test();
+    }
+  }, [])
 
   const iframe =
     '<iframe src="/xpla-academy-dev/img/web-desktop-latest/index.html" width="100%" height="100%"></iframe>';
