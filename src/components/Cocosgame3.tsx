2639c0f6857ba08dac8a3c7f681805726748f8a2
nglsena0722c <nglsena0722@com2us.com>
2023-11-20 10:09:48 +0900

    Test : Test web crypto api

--- src/components/Cocosgame3.tsx
+++ src/components/Cocosgame3.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 export default function Cocosgame2() {
-  const iframe = '<iframe src="/xpla-academy-dev/img/web-desktop-latest/index.html" width="100%" height="100%"></iframe>';
+  const test = async () => {
+    const encoder = new TextEncoder();
+    const data = encoder.encode("hihi");
+    const hash = await crypto.subtle.digest("SHA-256", data);
+    console.log('test')
+    console.log('isit ok?', hash);
+  };
+  test();
+
+  const iframe =
+    '<iframe src="/xpla-academy-dev/img/web-desktop-latest/index.html" width="100%" height="100%"></iframe>';
   return (
     <div
       style={{ width: 1000, height: 800 }}
