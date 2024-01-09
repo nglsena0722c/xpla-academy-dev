370da4721d0a229e571d475ed4ba2aa7efbf7265
nglsena0722c <nglsena0722@com2us.com>
2024-01-09 16:41:20 +0900

    Fix : Fix Footer

--- src/theme/Footer/Layout/index.js
+++ src/theme/Footer/Layout/index.js
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import styles from "./styles.module.css";
 import Link from "@docusaurus/Link";
 import NorthIcon from '@mui/icons-material/North';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const LinkList = [
   {
@@ -26,7 +27,7 @@ const LinkList = [
     title: 'COMMUNITY',
     content: [
       {
-        description: 'TWITTER',
+        description: 'X',
         link: 'https://twitter.com/XPLA_Official'
       },
       {
@@ -44,7 +45,7 @@ const LinkList = [
     content: [
       {
         description: 'XPLA DOCS',
-        link: 'https://docs.xpla.io/docs/learn/about-xpla-chain/'
+        link: 'https://docs.xpla.io/learn/learn/about-xpla-chain/'
       },
       {
         description: 'XPLA.JS',
@@ -52,11 +53,11 @@ const LinkList = [
       },
       {
         description: 'XPLAD',
-        link: 'https://docs.xpla.io/docs/develop/tools/xplad/about-xplad/'
+        link: 'https://docs.xpla.io/develop/develop/tools/xplad/about-xplad/'
       },
       {
         description: 'ENDPOINTS',
-        link: 'https://docs.xpla.io/docs/full-node/resources/public-and-private-endpoints/'
+        link: 'https://docs.xpla.io/full-node/full-node/resources/public-and-private-endpoints/'
       },
       {
         description: 'XPLA FAUCET',
@@ -92,22 +93,26 @@ const LinkList = [
 ];
 
 
-export default function FooterLayout({ style, links, logo, copyright }) {
+export default function FooterLayout({ style, links, logo, copyright, scrollToTop = undefined }) {
+  const matches = useMediaQuery('(max-width:768px)');
+
   return (
     <footer
-      className={clsx('bg-[#000000] h-[510px] flex flex-col p-0 items-center')}>
-      <div className="max-w-[1180px] w-[100%] py-10">
+      className={clsx('bg-[#000000] md:h-[510px] flex flex-col py-0 items-center overflow-hidden')}>
+      <div className="max-w-[1212px] w-[100%] px-4 py-10">
 
 
-        <div className="flex justify-between">
+        <div className="md:flex md:justify-between grid grid-cols-2 gap-8">
           {
             LinkList.map((LinkSubject, subjectIdx) => (
-              <div key={subjectIdx} className="flex flex-col gap-9 leading-tight">
-                <span className="text-[#D9D9D9] font-bold text-[18px]">{LinkSubject.title}</span>
+              <div key={subjectIdx} className="flex flex-col gap-4 md:gap-9 leading-tight">
+                <span className="text-[#D9D9D9] font-bold text-[20px] md:text-[18px]">{LinkSubject.title}</span>
                 <div className="flex flex-col gap-3">
                   {
                     LinkSubject.content.map((LinkInfo, infoIdx) => (
-                      <Link key={infoIdx} to={LinkInfo.link} className="hover:opacity-60 transition-all" style={{ textDecoration: 'none' }}>
+                      <Link 
+                      aria-label="footer-link"
+                      key={infoIdx} to={LinkInfo.link} className="hover:opacity-60 transition-all" style={{ textDecoration: 'none' }}>
                         <span className="text-[#D9D9D9] font-bold text-[16px]">{LinkInfo.description}</span>
                       </Link>
                     ))
@@ -116,34 +121,43 @@ export default function FooterLayout({ style, links, logo, copyright }) {
               </div>
             ))
           }
-          <div onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-            className="bg-[#00b2fc] w-[80px] h-[80px] flex justify-center items-center hover:cursor-pointer hover:opacity-60 transition-all">
+          {
+            !matches &&
+          <div onClick={async () => {
+            if (scrollToTop) {
+              await scrollToTop();
+            } else {
+              window.scrollTo({ top: 0, behavior: 'smooth' })
+            }
+          }}
+          className="bg-[#00b2fc] w-[80px] h-[80px] flex justify-center items-center hover:cursor-pointer hover:opacity-60 transition-all">
             <NorthIcon sx={{ fontSize: 50 }} />
           </div>
+            }
         </div>
 
       </div>
       <div className={clsx(styles.footerBorder)} />
-
-      <div className="max-w-[1180px] w-[100%] mt-10 flex justify-between">
-        <div className="flex gap-[80px] items-start">
-          <Link href="https://xpla.io" target="_blank" rel="noopener noreferrer" className={styles.xplalogo} ></Link>
+      
+      <div className="max-w-[1212px] px-4 w-[100%] mt-10 flex justify-between flex-col md:flex-row py-5 md:py-0">
+        <div className="flex gap-5 md:gap-[80px] items-start flex-col md:flex-row">
+          <Link aria-label='xpla.io' href="https://xpla.io" target="_blank" rel="noopener noreferrer" className={styles.xplalogo} ></Link>
           <div className="flex flex-col gap-[18px]">
             <div className="leading-tight text-[#D9D9D9] font-medium text-[16px]">
               <div>Universal Content Powerhouse for a Sublime</div>
-              <div>Creative Expreience.</div>
+              <div>Creative Experience.</div>
             </div>
-            <div className="text-[#D9D9D9] font-normal text-[13px]">Copyright © 2023 XPLA Acadmey. All rignts reserverd.</div>
+            <div className="text-[#D9D9D9] font-normal text-[13px]">Copyright © 2023 XPLA ACADEMY. All rights reserved.</div>
           </div>
 
-          <div className="flex gap-[14px]">
-            <Link href="https://github.com/xpladev" target="_blank" rel="noopener noreferrer" className={styles.footerGithubLink} ></Link>
-            <Link href="https://linktr.ee/xpla_official" target="_blank" rel="noopener noreferrer" className={styles.footerLinktree} ></Link>
+          <div className="flex gap-[14px] flex-1 justify-end">
+            <Link aria-label='xpla-github' href="https://github.com/xpladev" target="_blank" rel="noopener noreferrer" className={styles.footerGithubLink} ></Link>
+            <Link aria-label='xpla-linktr' href="https://linktr.ee/xpla_official" target="_blank" rel="noopener noreferrer" className={styles.footerLinktree} ></Link>
           </div>
         </div>
 
 
-        <div className="flex gap-[14px]">
+        {/* <div className="flex gap-[14px]">
           <Link style={{ textDecoration: 'none' }} className="h-fit transition-all text-[#D9D9D9] hover:text-[#D9D9D9] hover:cursor-pointer hover:opacity-60 font-bold text-[15px] ">
             Privacy Policy
           </Link>
@@ -155,7 +169,7 @@ export default function FooterLayout({ style, links, logo, copyright }) {
           <Link style={{ textDecoration: 'none' }} className="h-fit transition-all text-[#D9D9D9] hover:text-[#D9D9D9] hover:cursor-pointer hover:opacity-60 font-bold text-[15px]">
             Terms of Use
           </Link>
-        </div>
+        </div> */}
 
       </div>
     </footer>
